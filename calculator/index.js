55d7d2193de8264a5700df460e82e0b7b8966dc9
Krishanu Bhattachajee <imsonukrishanu111999@gmail.com>
2021-04-29 11:33:37 +0530

    completed

---
+++ calculator/index.js
@@ -0,0 +1,28 @@
+let screen = document.getElementById('screen');
+let button = document.querySelectorAll('button');
+let screenValue = '';
+for (item of button) {
+    item.addEventListener('click', (e) => {
+        let buttonText = e.target.innerText;
+        if (buttonText == 'X') {
+            buttonText = '*'
+            screenValue += buttonText;
+            screen.value += buttonText;
+        } else if (buttonText == 'C') {
+            screen.value = '';
+            screenValue = '';
+        } else if (buttonText == '=') {
+            try {
+                screen.value = eval(screenValue);
+            } catch (error) {
+                screen.value = 'ERROR!!'
+                alert("ERROR !! Please Enter Right Values...")
+            }
+        }
+        else {
+            screenValue += buttonText;
+            screen.value = screenValue;
+        }
+    })
+}
+
